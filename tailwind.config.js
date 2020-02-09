2f99aed7390cab3a08a5d482eca91742af04254a
Zach <zjwgibsg@gmail.com>
2020-02-09 12:32:58 -0500

    Heroku | First commit

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  theme: {
+    fontFamily: {
+      heroku: ["Salesforce Sans", "Helvetica"]
+    },
+    extend: {}
+  },
+  variants: { cursor: ["responsive", "hover", "focus"] },
+  plugins: []
+};
